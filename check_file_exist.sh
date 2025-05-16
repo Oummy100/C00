923e7752e85c6370a01849026adcf3333ce127cf
Oummy100 <mamoudousouleyh98@gmail.com>
2025-05-16 15:27:50 +0100

    Je suis repêchée

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
 # Demander à l'utilisateur d'entrer le nom du fichier
 echo "Entrez le nom du fichier :"
 read filename
-
 # Vérification de l'existence du fichier
 if [ -f "$filename" ]; then
   echo "✅ Le fichier '$filename' existe."
