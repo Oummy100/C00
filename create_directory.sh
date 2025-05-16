0e24670b49948700058a42b85810b1cfdd293498
Oummy100 <mamoudousouleyh98@gmail.com>
2025-05-16 15:22:04 +0100

    ça progresse

---
+++ create_directory.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Script pour créer un répertoire nommé test_directory
+echo "Entrez le nom du dossier:"
+read directoryname
+mkdir $directoryname
+echo "Le répertoire '$directoryname' a été créé."
+
